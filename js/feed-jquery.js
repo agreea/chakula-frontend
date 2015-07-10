e99f8587fe705ca7bfd462dcedc492268d755ae6
agreea <agreea@github.com>
2015-07-10 04:36:04 -0400

    transfer order name and price

--- js/feed-jquery.js
+++ js/feed-jquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function(event){
     $( ".item" ).click(function() {
-        window.location.href="order.html" + "#floralshoppe";
+        window.location.href="order.html" + "#" + "&" + ;
     });
 });
