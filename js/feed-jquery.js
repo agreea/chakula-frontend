9d0cbc7911c3a2958895fa79cc16fc3e648a7687
agreea <agreea@github.com>
2015-07-10 04:46:37 -0400

    transfer order name and price

--- js/feed-jquery.js
+++ js/feed-jquery.js
@@ -2,9 +2,9 @@ $(document).ready(function(event){
     $( ".item" ).click(function() {
     	    alert("order.html" + "#" + 
         	$(this).find(".item-title").text().split(' ').join("_") + "+" + // extract title with underscores
-        	$(this).find(".item-price").text().substring(str.slice(1)); // extract price without "$")
+        	$(this).find(".item-price").text().slice(1)); // extract price without "$")
         window.location.href = "order.html" + "#" + 
         	$(this).find(".item-title").text().split(' ').join("_") + "+" + // extract title with underscores
-        	$(.this).find(".item-price").text().substring(str.lastIndexOf("$")+1);
+        	$(.this).find(".item-price").text().slice(1);
     });
 });
