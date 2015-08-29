d05387cf6e022c3c8de308937c0968a6240c43f8
agreea <agreea@github.com>
2015-08-29 12:39:49 -0400

    redesign

--- js/feed-jquery.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(event){
-    $( ".item" ).click(function() {
-        window.location.href = "order.html" + "#" + 
-        	$(this).find(".item-title").text().split(' ').join("_") + "+" + // extract title with underscores
-        	$(this).find(".item-price").text().slice(1);
-    });
-});
