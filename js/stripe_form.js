cf7aa4031665c3b0f4eb90485002aad05ad10658
agreea <agreea@github.com>
2015-09-09 17:52:41 -0400

    Stripe form testing

--- js/stripe_form.js
+++ js/stripe_form.js
@@ -28,12 +28,12 @@
     };
 
     jQuery(function($) {
-      $('#payment-form').submit(function(e) {
-        var $form = $(this);
+      $('#payment-btn').click(function(e) {
+        var $form = $('#payment-form');
         // Disable the submit button to prevent repeated clicks
         console.log("Disabled submit...!")
         $form.find('button').prop('disabled', true);
-        // Stripe.card.createToken($form, stripeResponseHandler);
+        Stripe.card.createToken($form, stripeResponseHandler);
         // Prevent the form from submitting with the default action
         return true;
       });
