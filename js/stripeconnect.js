fefa7e167e6f3dbdbb7149e10e4c3ada08e3b4d5
agreea <agreea@github.com>
2015-09-09 11:32:23 -0400

    Created login testing page

--- js/stripeconnect.js
+++ js/stripeconnect.js
@@ -1,15 +1,31 @@
 $(document).ready(function() {
-	params = getUrlVars();
-	// resp = api_call('mealrequest', {
- //                method: "GetRequest",
- //                requestId: params.Id,
- //					sessionId: get s
- //            });
 	// get error, if it's not null show an error message and return
 	// get authorization code
+	code = params.code
+	session = getCookie("session")
+	if (session ===  ""){
+		// login flow
+		// then send Stripe data
+	}
 	// if the user doesn't have a session cookie, ask him/her to Login
-	// once they login, successfully, send over the Stripe data
-	// send it to server using 'host' { method: "StripeConnect", auth: params.Authorization_code }
+	// once they login, successfully, send over the Stripe data (api call)
+	// send it to server using 'host' { method: "StripeConnect", auth: params.Authorization_code, session: session }
 	// if the response is a success, show it
 	// if the response is a failure, show it
 );
+
+function sendStripeData() {
+	params = getUrlVars();
+	session = getCookie("session")
+	resp = api_call('host', {
+                method: "StripeConnect",
+                auth: params.code,
+ 				sessionId: session 
+            });
+	if (resp.Success) {
+		// show it
+		console.log("We did it!")
+	} else {
+		// show failure
+	}
+}
