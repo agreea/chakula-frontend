12e20f319f4362a5b8cbed4657e6af7976818c58
agreea <agreea@github.com>
2015-09-09 23:27:00 -0400

    put js in header

--- js/stripe_form.js
+++ js/stripe_form.js
@@ -11,11 +11,11 @@
         // token contains id, last4, and card type
         var token = response.id;
         console.log("Stripe response: " + token);
-        // var api_resp = api_call("kitchenuser", {
-        //               method: "AddStripe",
-        //               session: Cookies.get("session"),
-        //               StripeToken: token
-        //               });
+        var api_resp = api_call("kitchenuser", {
+                      method: "AddStripe",
+                      session: Cookies.get("session"),
+                      StripeToken: token
+                      });
         if (api_resp.Success) {
           // Show the meal has been requested
           console.log(api_resp.Return)
@@ -33,7 +33,7 @@
         // Disable the submit button to prevent repeated clicks
         console.log("Disabled submit...!")
         $form.find('button').prop('disabled', true);
-        Stripe.card.createToken($form, stripeResponseHandler);
+        // Stripe.card.createToken($form, stripeResponseHandler);
         // Prevent the form from submitting with the default action
         return false;
       });
