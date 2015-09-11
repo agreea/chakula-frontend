c73d9bcafeaf7e889e014185ba1477cd72ddf48f
agreea <agreea@github.com>
2015-09-10 22:44:10 -0400

    Adjusted listener

--- js/load-invoice.js
+++ js/load-invoice.js
@@ -5,15 +5,12 @@ $('#subtotal-price').text("$" + 10.50);
 $('#processing-price').text()
 $('#total-price')
 $('#last-4').text("..." + cards[cards.length - 1])
-    <p id="last-4">...<i>6399</i></p>
 
 // get the meal id
 // get the meal's cost (in cents)
 // add 2.9% + 30 cents
 // show dat
-$('#request-meal-btn').click(sendRequest);
-
-var sendRequest = function() {
+$('modal-body').find('#request-meal-btn').click(function() {
     var api_resp = api_call("kitchenRequest", {
                       method: "sendRequest",
                       session: Cookies.get("session"),
@@ -26,4 +23,4 @@ var sendRequest = function() {
     } else {
       // show that the request didn't process, ask them to try again
     }
-}
+})
