b7b7a6897f9e96633b3cc2c9ae21a86a42ad858f
agreea <agreea@github.com>
2015-09-10 22:25:08 -0400

    fixed button disable bug

--- js/stripe_form.js
+++ js/stripe_form.js
@@ -12,7 +12,7 @@
         // token contains id, last4, and card type
         var token = stripe_resp.id;
         console.log("Stripe response: " + token);
-        console.log("Stripe last 4: " + stripe_resp)
+        console.log(stripe_resp)
         var api_resp = api_call("kitchenuser", {
                       method: "AddStripe",
                       session: Cookies.get("session"),
