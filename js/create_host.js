c928a4b6498b4f29897198f77f011fc42dae3a3e
agreea <agreea@github.com>
2015-09-16 01:37:37 -0400

    add host data fields is there's a session

--- js/create_host.js
+++ js/create_host.js
@@ -3,7 +3,7 @@ function setupHostData(guestData) {
 	$host_data.load('include/host_data.html');
 	console.log(guestData.First_name);
 	$host_data.find('input[id="first-name"]').val(guestData.First_name);
-	$host_data.find('#last-name').val(guestData.Last_name);
+	$('#last-name').val(guestData.Last_name);
 	$host_data.find('#email').val(guestData.Email);
 	$("input[type='text']").change(function() {
 		console.log("detected a disturbance in the force");
