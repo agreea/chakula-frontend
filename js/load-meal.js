7a49f598616d0b06a1d4fb3cb43728676a6f258e
agreea <agreea@github.com>
2015-09-20 10:43:24 -0400

    RSVP-by testing

--- js/load-meal.js
+++ js/load-meal.js
@@ -87,21 +87,22 @@ function processDates(start_time, rsvp_by) {
   day = hour * 24;
   time_left = rsvp_by - Date.now()
   if (time_left > day) {
-    // $('#time-left').text(time_left/day)
-    // $('#time-left-subtext').text('days til requests close.')
-    // show how many days left
+    $('#time-left').text(Math.round(time_left/day));
+    $('#time-left-subtext').text('days til requests close.');
   } else if (time_left > hour) {
-    // $('#time-left').text(time_left/hour)
-    // $('#time-left-subtext').text('hours til requests close.')
-    // $('#time-left-subtext').css("color", "#00ff00") // set text to yellow
+    $('#time-left').text(Math.round(time_left/hour));
+    $('#time-left-subtext').text('hours til requests close.');
+    $('#time-left-subtext').css("color", "##FFCC00") // set text to yellow
   } else if (time_left > minute) {
-    // $('#time-left').text(time_left/hour)
-    // $('#time-left-subtext').text('minutes til requests close.')
-    // $('#time-left-subtext').css("color", "#ff0000") // set text to yellow
+    $('#time-left').text(Math.round(time_left/hour));
+    $('#time-left-subtext').text('minutes til requests close.');
+    $('#time-left-subtext').css("color", "#ff0000") // set text to red
   } else if (time_left < 0) {
-    // $('#time-left').text(0)
-    // $('#time-left-subtext').text('Requests are now closed.')
-    // $('#time-left-subtext').css("color", "#aaa") // set text to yellow
+    $('#time-left').html('<span class="glyphicon glyphicon-ban-circle" aria-hidden="true"></span>') 
+    $('#time-left-subtext').text('Requests are now closed.')
+    $('#time-left-subtext').css("color", "#aaa") // set text to grey
+    request_button = $('#request-meal-btn').prop('disabled', true)
+
   }
   // if rsvp_by - now > 24 hours ==> "# days left"
   // else if rsvp_by - now > 1 hour ==> "# hours left" (text is yellow)
