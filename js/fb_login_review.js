304cc4eac205a392a8db55b52e0b9d7132982050
agreea <agreea@github.com>
2015-09-25 02:23:05 -0400

    hide fb button on login success

--- js/fb_login_review.js
+++ js/fb_login_review.js
@@ -23,6 +23,7 @@
             console.log("Here is the result: " + resp.Return.Session_token);
             Cookies.set("session", resp.Return.Session_token, { expires: 50 });
             $('#review-ui').show();
+            $('#fb').hide();
             console.log("from cookie: " + Cookies.get("session"))
           }
       } else {
