f830b48b9d401b4d7c084c394f0644da27dbfb45
agreea <agreea@github.com>
2015-09-24 22:51:27 -0400

    Reviews debug

--- js/load-meal.js
+++ js/load-meal.js
@@ -197,9 +197,9 @@ function getCards() {
 
 function setupReviews(reviews) {
   for (i in reviews) { 
-    stars_html = ''
+    stars_html = '';
     for (j = 0; j < reviews[i].Rating; j++) {
-      stars_html +='<span class="glyphicon glyphicon-star" aria-hidden="true"></span>'
+      stars_html +='<span class="glyphicon glyphicon-star" aria-hidden="true"></span>';
     }
     $('<div class="row"><div class="col-sm-3 text-center col-sm-offset-1"><img class="img-circle guest-pic" id="guest-pic" src="'+ 
       reviews[i].Prof_pic_url +'"><p class="reviewer-name">'+ 
