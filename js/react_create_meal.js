e50d05693b45e73226fde532c174c77ca01e701a
agreea <agreea@github.com>
2015-10-24 04:25:16 -0400

    saving animation

--- js/react_create_meal.js
+++ js/react_create_meal.js
@@ -347,9 +347,9 @@ var attemptSave = function() {
         mealId: urlVars['Id']
   };
   console.log(data);
-  $(this).html('<p>Saving </p><div class="spinner"><div class="bounce1">' + 
+  $(this).html('<p>Saving </p><span class="spinner"><div class="bounce1">' + 
               '</div><div class="bounce2"></div>' +
-              '<div class="bounce3"></div></div>');
+              '<div class="bounce3"></div></span>');
   var api_resp = api_call('meal', data);
   $(this).prop('disabled', true);
   if (api_resp.Success) {
