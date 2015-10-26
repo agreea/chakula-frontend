414dd94f60d983ccb78c6d1bc197738ef56b974a
agreea <agreea@github.com>
2015-10-26 07:07:28 -0400

    added final invoice screen

--- js/fb_login.js
+++ js/fb_login.js
@@ -83,13 +83,12 @@
       //   statusChangeCallback(response);
       // });
   };
-
   // Load the SDK asynchronously
   (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=828767043907424";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
