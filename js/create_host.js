779af8e5783b8732c2196685d301fd459974bf2a
agreea <agreea@github.com>
2015-10-27 16:46:32 -0400

    testing load meal

--- js/create_host.js
+++ js/create_host.js
@@ -1,49 +1,88 @@
-function getHostData() {
-  return api_call('host', {
-                    method: 'GetHost',
-                    session: Cookies.get('session')});
-}
+      var phone = "";
+      var address = "";
+      var email = "";
+      var FormTextRow = React.createClass({
+        getInitialState: function() {
+          return ({value: this.props.default_val});
+        }
+        formChanged: function(e) {
+          setState({value: e.target.result});
+        },
+        render: function() {
+          var default_val = "";
+          if (this.props.default_val) {
+            default_val = this.props.default_val;
+          }
+          return (<div className="row">
+              <div className="col-xs-4 col-sm-2 form-label">
+                <p className="text-right">{this.props.form_name}</p>
+              </div>
+              <div className="col-sm-8">
+              <input className="text-field" id={this.props.id} type="text" 
+                defaultValue={default_val} 
+                onChange={this.formChanged}
+                placeholder={this.props.placeholder}/>
+              </div>
+            </div>);
+        }
+      });
+      
+      var ProfileForm = React.createClass({
+        emailChanged: function(e){
+          email = e.target.result;
+          this.setState({Email: e.target.result}); 
+        },
+        addressChanged: function(e) {
+          address = e.target.result;
+          this.setState({Address: e.target.result}); 
+        },
+        phoneChanged: function(e) {
+          phone = e.target.result;
+          this.setState({Phone: e.target.result}); 
+        },
+        render: function() {
+          return (<div className="row">
+          	<h3 className="text-center">Your Host Profile</h3>
+              <FormTextRow form_name="Email" 
+                place_holder="One you actually check" 
+                id="email"
+                default_val={this.props.data.Email}/>
+              <FormTextRow form_name="Phone #" 
+                place_holder="01234567890" 
+                id="phone"
+                default_val={this.props.data.Phone}/>
+              <FormTextRow form_name="Address" 
+                place_holder="3700 O St NW" 
+                id="address"/>
+              <div className="row">
+                <div className="col-xs-4 col-sm-2">
+                  <p className="text-right">Payment</p>
+                </div>
+                <button type="button" class="btn-info btn-lg btn" onClick={this.attemptSendHostData}>Save</button>
+                <div className="col-xs-6 col-sm-4">
+                  <a className="stripe-btn btn-lg btn" 
+                  	href="https://connect.stripe.com/oauth/authorize?response_type=code&amp;client_id=ca_6n8She3UUNpFgbv1sYtB28b6Db7sTLY6&amp;scope=read_write" 
+                  	target="_blank">Connect With Stripe</a>
+                </div>
+                <ul className="warning-field" id="stripe-warning">
+                </ul>
+              </div>
+            </div>);
+        }
+      });
 
-function setupHostData(hostData) {
-	$host_inputs = $('.host-data');
-	console.log(hostData.Prof_pic);
-	$('#prof-pic').attr("src", hostData.Prof_pic);
-	$host_inputs.find('#first-name').val(hostData.First_name);
-	$host_inputs.find('#last-name').val(hostData.Last_name);
-	$host_inputs.find('#email').val(hostData.Email);
-	$host_inputs.find('#phone').val(hostData.Phone);
-	$host_inputs.find('#address').val(hostData.Address);
-	if (!hostData.Stripe_connect) {
-		$('#stripe-warning').html(
-        "<li><span class='glyphicon glyphicon-cutlery' aria-hidden='true'></span>" +
-          "Please save your data BEFORE you connect with Stripe. You must connect with Stripe to host on Chakula." +
-          "</li>"
-			);
-	}
-	$("input").keypress(function() {
-		console.log("detected a disturbance in the force");
-		$host_inputs.find('button').prop('disabled', false);
-		// TODO: set text to "save", remove checkmark, restore old color styling
-    });
-	$host_inputs.find('button').click(function() {
-		attemptSendHostData();
-	});
+function create_host_render(guest) {
+    React.render(
+    	<ProfileForm data={guest}/>,
+        document.getElementById('host-data'));
+    }
 }
 
 function attemptSendHostData() {
 	console.log("attempting to send host data");
 	var submittable = true;
 	var errorHtml = ""
-	$host_data = $('.host-data');
-	console.log("First name @attemptSend:" + $host_data.find('#first-name').val());
-	if (!$host_data.find('#first-name').val()) {
-		submittable = false;
-		errorHtml += "<li>First name is mandatory</li>"
-	}
-	if (!$host_data.find('#last-name').val()) {
-		submittable = false;
-		errorHtml += "<li>Last name is mandatory</li>"
-	}
+	var $host_data = $('.host-data');
 	if (!$host_data.find('#email').val()) {
 		submittable = false;
 		errorHtml += "<li>Email is mandatory</li>"
@@ -74,22 +113,14 @@ function attemptSendHostData() {
 }
 
 function sendHostData() {
-	$host_data = $('.host-data');
-	$host_data.find('#first-name').val();
-	$host_data.find('#last-name').val();
-	$host_data.find('#email').val();
-	$host_data.find('#phone').val();
-	var firstName = $host_data.find('#first-name').val();
-	var lastName = $host_data.find('#last-name').val();
+	var $host_data = $('.host-data');
 	var email = $host_data.find('#email').val();
 	var phone = $host_data.find('#phone').val();
 	var address = $host_data.find('#address').val();
 	console.log("first: " + $host_data.find('#first-name').val() + ". Last: " + lastName + ". Email: " + email + ". Phone: " + phone + ". Address: " + address);
-	api_resp = api_call('host', {
+	var api_resp = api_call('host', {
 						method: 'updateHost',
 						session: Cookies.get('session'),
-						firstName: firstName,
-						lastName: lastName,
 						email: email,
 						phone: phone,
 						address: address
@@ -102,5 +133,18 @@ function sendHostData() {
 	    $host_data.find('button').css("background-color", "#19a347");
 	    $host_data.find('button').css("color", "#fff");
 		// show the saved button as green, add check mark, disable
+	}
+}
+
+if (!Cookies.get('session')) {
+  window.location.replace("http://yaychakula.com");
+}
+
+var api_resp = api_call('host', {method: 'get', session:Cookies.get('session')});
+
+if (!api_resp.Success || !api_resp.Return.Stripe_user_id) {
+	api_resp = api_call('kitchenuser', {method: 'get', session: Cookies.get('session')});
+	if (api_resp.Success) {
+		create_host_render(api_resp.Return);
 	}
 }
