6c54e73d791a9bf33afc328d1ca021835019de4d
agreea <agreea@github.com>
2015-11-12 03:29:22 -0500

    map radius solution

--- js/load-meal.js
+++ js/load-meal.js
@@ -149,7 +149,7 @@ var MealInfo = React.createClass({
         <div className="row">
           <p>{data.Address}</p>
         </div>
-        <div className="row map-row" style={{background-image: 'url("' + data.Maps_url + '")'}}>
+        <div className="row map-row" style={{backgroundImage: 'url("' + data.Maps_url + '")'}}>
           <svg className="map-radius" height="100" width="100">
             <circle cx="50" cy="50" r="40" stroke="#3C8BA6" stroke-width="2" fill="#6DCDED" opacity=".8" />
           </svg> 
