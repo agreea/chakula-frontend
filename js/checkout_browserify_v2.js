a8d1763508baf8523cf1d31f68797955345c69ec
agreea <agreea@github.com>
2015-12-10 12:20:38 -0500

    stripe publishable key

--- js/checkout_browserify_v2.js
+++ js/checkout_browserify_v2.js
@@ -3,6 +3,7 @@ module.exports = React.createClass({displayName: "exports",
 	handleAddCard: function() {
         this.setState({disableAddCard: true});
     	Stripe.setPublishableKey('pk_live_N4yRGKt9KwKwi9WfpAtnPdMs');
+    	console.log($('#cc-form'));
         Stripe.card.createToken($('#cc-form'), this.handleStripeResponse);
     },
     handleStripeResponse: function(status, stripe_resp) {
