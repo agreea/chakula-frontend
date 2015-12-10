097d7b6d86fe16da7212f47acc82999c2f015f57
agreea <agreea@github.com>
2015-12-09 20:19:37 -0500

    new modularizations for checkout

---
+++ js/add_card.js
@@ -0,0 +1,55 @@
+module.exports = React.createClass({
+	handleAddCard: function() {
+        this.setState({disableAddCard: true});
+        Stripe.card.createToken($('#cc-form'), this.handleStripeResponse);
+    },
+    handleStripeResponse: function(status, stripe_resp) {
+        if (stripe_resp.error) {
+            this.setState({disableAddCard: false, error: stripe_resp.error});
+            return;
+        }
+        this.submitTokenToServer(stripe_resp);
+    },
+    submitTokenToServer: function(stripe_resp) {
+        var api_resp = 
+            api_call("kitchenuser", {
+                method: "AddStripe",
+                session: Cookies.get("session"),
+                stripeToken: stripe_resp.id,
+                last4: stripe_resp.card.last4});
+        if (!api_resp.Success) {
+            this.setState({addCardError: api_resp.Error, disableAddCard: false});
+            return;
+        }
+        this.handleAddCardSuccess(stripe_resp.last4);
+    },
+	render: function() {
+		return (
+            <div className="row">
+                <form id="cc-form" className="col-sm-8">
+                    <span className="error-field" id="payment-errors"></span>
+                    <div className="form-row">
+                      <label>
+                         <input type="text" size="20" data-stripe="number" placeholder="Card #"/>
+                      </label>
+                    </div>
+                    <div className="form-row">
+                        <label>
+                            <input type="text" size="4" data-stripe="exp-month" placeholder="MM"/>
+                        </label>
+                        <span> / </span>
+                        <label>
+                            <input type="text" size="4" data-stripe="exp-year" placeholder="YY"/>
+                        </label>
+                        <span>  </span>
+                        <label>
+                             <input type="text" size="4" data-stripe="cvc" placeholder="CVC"/>
+                          </label>
+                    </div>
+                  <div className="text-center">
+                      <button className="brand-btn btn btn-payment" id="add-card-btn" onclick={this.handleAddCard} disabled={this.state.addCardDisabled}>Add Card</button>
+                  </div>
+                </form>
+            </div>);
+	}
+});
