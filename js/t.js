868b9c535a6ce8cf8f685e40ac975af9be255368
Agree <agree.ahmed@gmail.com>
2016-01-22 19:36:08 -0500

    home.js - deliberately broke build to test alert agree method

--- js/t.js
+++ js/t.js
@@ -1,4 +1,5 @@
-var webapp_validated = $("#js-working")
+var webapp_validated = $("#js-working");
+console.log(webapp_validated);
 if (!webapp_validated) {
 	api_call("kitchenuser", {method: "alertAgree", session: Cookies.get("session")});
 }
