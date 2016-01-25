cd6e4c6799a5a933d4ee302196c5f5294a9652b1
Agree <agree.ahmed@gmail.com>
2016-01-25 17:09:41 -0500

    t.js - added url to api call

--- js/t.js
+++ js/t.js
@@ -1,4 +1,8 @@
 var webapp_validated = document.getElementById("js-working");
 if (!webapp_validated) {
-	api_call("kitchenuser", {method: "alertAgree", session: Cookies.get("session")});
+	api_call("kitchenuser", {
+		method: "alertAgree", 
+		session: Cookies.get("session"),
+		url: window.location.href.split("yaychakula.com")[1]
+	});
 }
