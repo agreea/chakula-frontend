e49845a24775f0222731855d3acdcf40eeded737
Agree <agree.ahmed@gmail.com>
2016-03-01 00:00:32 -0500

    fix

--- js/pages/edit_guest_profile.js
+++ js/pages/edit_guest_profile.js
@@ -109,7 +109,7 @@ module.exports = React.createClass({
       <div className="row" id="edit-host-info">
         <div className="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 text-center">
           <h2>Log in To Chakula to Access Your Profile</h2>
-          <div className="text-center"
+          <div className="text-center">
             <Link to="/login?fwd=edit_guest_info">
               <button className="brand-btn c-blue-bg text-center">Login</button>
             </Link>
